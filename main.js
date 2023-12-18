48ed9fb150474ebb43f58378054dcc62ef0ea3b2
“horia-abdaljabbar” <horiahaji.77sr@gmail.com>
2023-12-18 09:55:27 +0300

    js-test

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".user").classList.add("demo");
+console.log(document.querySelector(".user"));
+
+
+
+console.log(document.querySelector("h1"));
+document.querySelector(".vip").classList.remove("vip");
+
